48ac330765458c1a6985eb764952001e2e40273b
kes <kmskes1125@gmail.com>
2023-05-05 21:35:27 +0900

    spring security를 이용해 토큰을 발급하고 저장함

--- src/main/java/com/example/miniproject/service/UserService.java
+++ src/main/java/com/example/miniproject/service/UserService.java
@@ -4,7 +4,9 @@ import com.example.miniproject.dto.LoginRequestDto;
 import com.example.miniproject.dto.SignupRequestDto;
 import com.example.miniproject.dto.BasicResponseDto;
 import com.example.miniproject.entity.User;
+import com.example.miniproject.jwt.JwtUtil;
 import com.example.miniproject.repository.UserRepository;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +16,7 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final JwtUtil jwtUtil;
     
     public BasicResponseDto<?> signup(SignupRequestDto signupRequestDto) {
         User user = new User(signupRequestDto);
@@ -22,7 +25,7 @@ public class UserService {
         return BasicResponseDto.setSuccess("회원 가입 완료!", null);
     }
 
-    public BasicResponseDto<?> login(LoginRequestDto loginRequestDto) {
+    public BasicResponseDto<?> login(LoginRequestDto loginRequestDto, HttpServletResponse response) {
         String userId = loginRequestDto.getUserId();
         String password = loginRequestDto.getPassword();
 
@@ -32,8 +35,10 @@ public class UserService {
 
         //비밀번호 일치여부
         if (!password.equals(user.getPassword())) {
-            return BasicResponseDto.setFailed("로그인 실패!");
+            return BasicResponseDto.setFailed("로그인 실패! 비밀번호가 일치하지 않습니다.");
         }
+
+        response.addHeader(JwtUtil.AUTHORIZATION_HEADER, jwtUtil.createToken(user.getUserId()));
 
         return BasicResponseDto.setSuccess("로그인 성공!", null);
     }
