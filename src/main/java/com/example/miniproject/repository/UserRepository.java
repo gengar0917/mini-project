0b175aa6a43f946f8b2d83f3390f0bd23f8aa7f7
kes <kmskes1125@gmail.com>
2023-05-05 20:04:51 +0900

    user 관련 내용 커밋

--- src/main/java/com/example/miniproject/repository/UserRepository.java
+++ src/main/java/com/example/miniproject/repository/UserRepository.java
@@ -3,7 +3,9 @@ package com.example.miniproject.repository;
 import com.example.miniproject.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    User findByUserId(String userId);
+    Optional<User> findByUserId(String userId);
 }
